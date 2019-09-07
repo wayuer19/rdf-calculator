8ccad37daa46b17b3ab619513a0b17c3599e50be
SL Lee <wayuer19@sjtu.edu.cn>
2019-09-07 20:05:39 +0800

    Update rdf-lam.py

--- rdf-lamellae/rdf-lam.py
+++ rdf-lamellae/rdf-lam.py
@@ -9,13 +9,13 @@ import linecache
 
 rho = 3.0
 delr = 0.1
-f_xyz = open("rdf.xyz","r")
+f_xyz = open("rdf.xyz","r")  ##input file
 f_dat = open("rdf.dat","w")
 
 lx,ly,lz = 200.00,40.00,40.00
 rxc,ryc,rzc = 0.00,0.00,0.00
-np = 3086
-maxbin = int(lx*2/delr)
+np = 3086   ##bead number
+maxbin = int(lx*2/delr)  ##apart the lamellae into thin slips
 
 rxyz = []
 for lines in f_xyz.readlines()[2:]:
@@ -23,6 +23,7 @@ for lines in f_xyz.readlines()[2:]:
     rxyz.append(line)
 f_xyz.close()
 
+##calculate the center of molecules
 for i in range(np):
     rxc = rxc + float(rxyz[i][1])
     ryc = ryc + float(rxyz[i][2])
@@ -50,12 +51,14 @@ for i in range(np):
     	 numO[bin_i] = numO[bin_i]+1
     nums[bin_i] += 1
 
+##calculate the volume density
 #vol = ly*lz*delr
 #for i in range(maxbin):	  
 #    numO[i] = numO[i]/vol
 #    numC[i] = numC[i]/vol
 #    print >> f_dat, i*delr, numO[i], numC[i]
 
+##calculate the number ratio
 for i in range(maxbin):
     if nums[i] != 0:
        fraO = numO[i]/nums[i]
